916ee0f7b005a4c86b8bbf294a9517ac4981fcdc
Xmas178 <xmas178@gmail.com>
2025-11-18 10:19:40 +0200

    Switch back to SQLite for deployment

--- database/__init__.py
+++ database/__init__.py
@@ -1,23 +1,15 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Get database URL from environment variable
-# Falls back to SQLite if DATABASE_URL is not set
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./shoe_store.db")
-
-# PostgreSQL doesn't need check_same_thread, but SQLite does
-connect_args = {}
-if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
-    connect_args = {"check_same_thread": False}
+# SQLite database
+SQLALCHEMY_DATABASE_URL = "sqlite:///./shoe_store.db"
 
 # Create engine (connection to database)
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},  # Required for SQLite
+)
 
 # Session (used for communicating with database)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
